f8573e2539682bdba990ab41f522c474593ea95d
Lincxx <bossco28@gmail.com>
2022-03-24 16:57:39 -0400

    Added a relations with models

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,7 +1,10 @@
 class Entry < ApplicationRecord
   validates :calories, :proteins, :carbohydrates, :fats, :meal_type, presence: :true
+  belongs_to :category
 
   def day 
     self.created_at.strftime("%b %e, %Y")
   end
+
+  
 end
