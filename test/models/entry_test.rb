8b1c1c5136cb71b630e3aa0fb71e03470efa7566
Lincxx <bossco28@gmail.com>
2022-03-24 06:50:27 -0400

    TTD testing

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -4,4 +4,30 @@ class EntryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "is valid with valid attributes" do
+    entry = Entry.new(meal_type: "Breakfast", carbohydrates: 35, proteins: 20, fats: 10, calories: 400)
+    assert entry.save
+  end
+
+  # test "should not save entry without calories" do
+  #   entry = Entry.new(carbohydrates: 35, proteins: 20, fats: 10)
+  #   assert_not entry.save
+  # end
+
+  # test "should not save entry without carbohydrates" do
+    
+  # end
+
+  # test "should not save entry without proteins" do
+    
+  # end
+
+  # test "should not save entry without fats" do
+    
+  # end
+
+  # test "should not save entry without meal_type" do
+    
+  # end
 end
